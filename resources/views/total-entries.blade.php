1ad16f5853bd4c2b7b8be87a25ea465abad54689
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-06-29 15:26:57 -0300

    All months added

--- resources/views/total-entries.blade.php
+++ resources/views/total-entries.blade.php
@@ -22,8 +22,18 @@
 <form class="form-control" action="list-month" method="post">
 	<input type="hidden" name="_token" value="{{{ csrf_token() }}}">
 	<input type="hidden" name="entry" value="{{$entry}}" >
+	<button type="submit" class="btn btn-primary" name="month" value="01">January</button>
+	<button type="submit" class="btn btn-primary" name="month" value="02">February</button>
+	<button type="submit" class="btn btn-primary" name="month" value="03">March</button>
+	<button type="submit" class="btn btn-primary" name="month" value="04">April</button>
 	<button type="submit" class="btn btn-primary" name="month" value="05">May</button>
 	<button type="submit" class="btn btn-primary" name="month" value="06">June</button>
+	<button type="submit" class="btn btn-primary" name="month" value="07">July</button>
+	<button type="submit" class="btn btn-primary" name="month" value="08">August</button>
+	<button type="submit" class="btn btn-primary" name="month" value="08">September</button>
+	<button type="submit" class="btn btn-primary" name="month" value="08">October</button>
+	<button type="submit" class="btn btn-primary" name="month" value="08">November</button>
+	<button type="submit" class="btn btn-primary" name="month" value="08">December</button>
 </form>
 
 @stop
